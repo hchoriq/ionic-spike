f0a7133bfb89ffec5b54fdf21c64f390deef3a80
Herman Chor <hermanc@iqmetrix.com>
2018-03-07 14:28:15 -0800

    Keystore, add temp error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Pro } from '@ionic/pro';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,6 +15,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      Pro.monitoring.exception(new Error('test error'))
       statusBar.styleDefault();
       splashScreen.hide();
     });
